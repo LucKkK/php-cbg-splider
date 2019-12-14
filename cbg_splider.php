7722a5fbc2a54fc5fc9da6b01877f03044d3ea86
luc <578147463@qq.com>
2019-12-14 17:54:18 +0800

    返回参数key

--- cbg_splider.php
+++ cbg_splider.php
@@ -4,11 +4,21 @@ function cbg(){
         $http = "https://recommd.xyq.cbg.163.com/cgi-bin/recommend.py?callback=Request.JSONP.request_map.request_0&_=".time()."&act=recommd_by_role&server_id=367&areaid=25&server_name=%E6%98%9F%E6%B5%B7%E6%B9%BE&page=1&query_order=selling_time%20DESC&kindid=0&view_loc=equip_list&count=50";
         $s = file_get_contents($http);
         echo $http;
+        $arr = array();
         $r=jsonp_decode($s,true);
         foreach ($r['equips'] as $k => $v) {
             $r['equips'][$k]['href'] = "https://xyq.cbg.163.com/equip?s=367&eid=".$v['eid'];
+            // foreach ($v as $m => $n) {
+            // 	if(!in_array($m, $arr)){
+            // 		$arr[] = $m;
+            // 	}
+            // }
         }
+// var_dump($arr);
         var_dump($r);
+        // foreach ($r['equips'] as $key => $value) {
+        // 	var_dump($value['highlights']);echo $value['href'];
+        // }
 }
 
 function jsonp_decode($jsonp, $assoc = false)
