980a99917050802485e29ac3c376a718566de874
luc <578147463@qq.com>
2019-12-17 14:58:52 +0800

    新增翻页

--- cbg_splider.php
+++ cbg_splider.php
@@ -3,22 +3,13 @@ function cbg(){
         header("Content-Type:text/html;charset=utf8");
         $http = "https://recommd.xyq.cbg.163.com/cgi-bin/recommend.py?callback=Request.JSONP.request_map.request_0&_=".time()."&act=recommd_by_role&server_id=367&areaid=25&server_name=%E6%98%9F%E6%B5%B7%E6%B9%BE&page=1&query_order=selling_time%20DESC&kindid=0&view_loc=equip_list&count=50";
         $s = file_get_contents($http);
-        echo $http;
+        // echo $http;
         $arr = array();
         $r=jsonp_decode($s,true);
         foreach ($r['equips'] as $k => $v) {
             $r['equips'][$k]['href'] = "https://xyq.cbg.163.com/equip?s=367&eid=".$v['eid'];
-            // foreach ($v as $m => $n) {
-            // 	if(!in_array($m, $arr)){
-            // 		$arr[] = $m;
-            // 	}
-            // }
         }
-// var_dump($arr);
-        var_dump($r);
-        // foreach ($r['equips'] as $key => $value) {
-        // 	var_dump($value['highlights']);echo $value['href'];
-        // }
+        return $r['equips'];
 }
 
 function jsonp_decode($jsonp, $assoc = false)
@@ -39,5 +30,8 @@ function jsonp_decode($jsonp, $assoc = false)
         return json_decode($jsonp, $assoc);
     }
 
-cbg();
+$result = cbg();
+if (isset($_GET['p'])) {
+	var_dump($result);
+}
 
---
+++ show.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>show</title>
+	<meta charset="utf-8">
+	<script src="https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+</head>
+<body>
+
+<?php require_once("./cbg_splider.php"); ?>
+<?php require_once("./cbg_dec.php"); ?>
+
+<?php $i=1;foreach ($result as $k => $v) { ?>
+<div style="<?php if($i != 1){echo "display: none;";}?>" id="data_<?php echo $i;?>" class="data_page">
+	<?php foreach ($v as $m => $n) {?>
+	<div style="background-color: ; width: 33%; border: 2px;;">
+		<p><?php if($m=='href'){?><a href="<?php echo $n;?>" target="view_window"><?php }?><?php echo $all_dec[$m].':   ';if(!is_array($n)){echo $n;}else{var_dump($n);}?><?php if($m=='href'){?></a><?php }?></p>
+	</div>
+	<?php }?>	
+</div>
+<?php $i++;}?>
+
+<input id="page_num" value="1" style="display: none;">
+<button id="up">上一页</button>
+<button id="down">下一页</button>
+
+<script type="text/javascript">
+	$("#up").click(function(){
+		var page_num = $("#page_num").val();
+		page_num--;
+
+		if(page_num < 1){
+			page_num = 1;
+		}
+
+		$("#page_num").val(page_num);
+		$(".data_page").hide();
+		$("#data_"+page_num).show();
+	})
+	$("#down").click(function(){
+		var page_num = $("#page_num").val();
+		page_num++;
+		$("#page_num").val(page_num);
+		$(".data_page").hide();
+		$("#data_"+page_num).show();
+	})
+</script>
+
+
+
+</body>
+</html>
